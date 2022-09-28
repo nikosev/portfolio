1dcfd3ed0ad8f78e6054422a5d5d0c82ad9023e5
nikosev <nikos.ev@hotmail.com>
2022-09-28 21:14:00 +0000

    Deploying to gh-pages from  @ 30f9356cac0f1a045f6075ff5e6bb44ff07f3ffc 🚀

---
+++ precache-manifest.9807387023f85917c678da68a7cff7aa.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff293b967b44e25f3ddd0e5abdc8c17",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b3dd89a18cdeb7c0c010",
+    "url": "/portfolio/static/css/3.a28360d6.chunk.css"
+  },
+  {
+    "revision": "136ef288a0c69af44a6f",
+    "url": "/portfolio/static/css/4.7d5c10e0.chunk.css"
+  },
+  {
+    "revision": "949eb4f2f44427cd99e6",
+    "url": "/portfolio/static/css/main.b028cad9.chunk.css"
+  },
+  {
+    "revision": "bc739129970dfea9d651",
+    "url": "/portfolio/static/js/2.250dc4f0.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/portfolio/static/js/2.250dc4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3dd89a18cdeb7c0c010",
+    "url": "/portfolio/static/js/3.3ef5d3f3.chunk.js"
+  },
+  {
+    "revision": "136ef288a0c69af44a6f",
+    "url": "/portfolio/static/js/4.27681039.chunk.js"
+  },
+  {
+    "revision": "949eb4f2f44427cd99e6",
+    "url": "/portfolio/static/js/main.cf225f47.chunk.js"
+  },
+  {
+    "revision": "825277e9939c6fd48f33",
+    "url": "/portfolio/static/js/runtime-main.743977e8.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "a60aaec016e4f3a4568e1bd0b8393889",
+    "url": "/portfolio/static/media/grnetShortLogo.a60aaec0.png"
+  },
+  {
+    "revision": "610953ae5ac521ccf4a421c43815cb69",
+    "url": "/portfolio/static/media/haiscLogo.610953ae.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "cdc44f0a3266ff6cf3539166738fa24a",
+    "url": "/portfolio/static/media/universityOfPiraeusLogo.cdc44f0a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9e7d619a6d439fb183f930a10e46120.js"
+  "/portfolio/precache-manifest.9807387023f85917c678da68a7cff7aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

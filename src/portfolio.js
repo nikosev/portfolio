60b63693f171cb1362ac8c323c61f2c665258019
nikosev <nikos.ev@hotmail.com>
2022-12-01 00:37:04 +0200

    Enable `achievementSection`

--- src/portfolio.js
+++ src/portfolio.js
@@ -298,7 +298,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
